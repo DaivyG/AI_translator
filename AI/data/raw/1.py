f130711d804ad674dc55ceb33aea7e82127b3602
Dayvi <guseynov.davidka@mail.ru>
2024-07-16 01:00:03 +0300

    все данные привел в порядок

--- AI/data/raw/1.py
+++ AI/data/raw/1.py
@@ -1,24 +1,12 @@
 import pandas as pd
-import csv
 
-# Пример исходных данных
-with open('./AI/data/raw/numbers.txt') as f:
-    data = f.read()
+# Замените 'input.xlsx' на путь к вашему Excel файлу
+excel_file_path = './AI/data/raw/phrases.xlsx'
+# Замените 'output.csv' на желаемое имя выходного CSV файла
+csv_file_path = './AI/data/processed/phrase.csv'
 
-# Разделение на строки и обработка данных
-lines = data.strip().split('\n')
-cleaned_data = []
+# Чтение Excel файла
+df = pd.read_excel(excel_file_path)
 
-# Проход по строкам и извлечение пар "кубачинский - русский"
-for line in lines:
-    russian, kubanch = map(str.strip, line.split(' – '))
-    cleaned_data.append((russian, kubanch))
-
-# Преобразование в DataFrame
-df = pd.DataFrame(cleaned_data, columns=['Russian', 'Kubachin'])
-
-# Сохранение данных в CSV-файл
-df.to_csv('./AI/data/processed/numbers.csv', index=False, encoding='utf-8')
-
-# Проверка результата
-print(df)
+# Сохранение в CSV формате
+df.to_csv(csv_file_path, index=False)
